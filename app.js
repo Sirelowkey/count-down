bc8038f6c9a03574451d436b99e8b7409f4132d0
anything <101835549+Sirelowkey@users.noreply.github.com>
2022-04-27 06:00:54 -0400

    counter

---
+++ app.js
@@ -0,0 +1,29 @@
+let header = document.querySelector('h1')
+let dec = document.querySelector('.dec')
+let res = document.querySelector('.res')
+let inc = document.querySelector('.inc')
+let body = document.querySelector('body')
+
+
+function decrease () {
+    header.textContent --
+    body.style.backgroundColor = 'yellow'
+
+}
+
+dec.addEventListener('click',decrease)
+
+function increase () {
+    header.textContent ++
+    body.style.backgroundColor ='green'
+}
+
+inc.addEventListener('click',increase)
+
+function reset () {
+    header.textContent = 0
+    body.style.backgroundColor ='red'
+}
+res.addEventListener('click', reset)
+
+
